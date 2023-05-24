119d46a35b404d7f3af54a6ec556747544e4ff15
micha <michal.koscinskiilo@gmail.com>
2023-05-24 10:21:57 +0200

    Export v2 with procedures

--- Stronka/app/Http/Controllers/ExportController.php
+++ Stronka/app/Http/Controllers/ExportController.php
@@ -9,35 +9,46 @@ use App\Models\Patient;
 use App\Models\Ward;
 use Illuminate\Http\Request;
 use Laracsv\Export;
+use Illuminate\Support\Facades\DB;
 
 class ExportController extends Controller
 {
     public function exportAddresses() {
-        $data = Address::all();//TODO:zrobic procedure ktora bedzie wykonywac export
+        $data = DB::select('EXEC ExportData ?',['addresses']);
+
+        $data= collect($data);
 
         $csvExporter = new Export();
         $csvExporter->build($data, ['id', 'voivodeship', 'city', 'street', 'house_number','flat_number', 'postal_code','created_at', 'updated_at'])->download();
     }
     public function exportIllnesses() {
-        $data = Illness::all();//TODO:zrobic procedure ktora bedzie wykonywac export
+        $data = DB::select('EXEC ExportData ?',['illnesses']);
+
+        $data= collect($data);
 
         $csvExporter = new Export();
         $csvExporter->build($data, ['id', 'name', 'description'])->download();
     }
     public function exportDoctors() {
-        $data = Doctor::all();//TODO:zrobic procedure ktora bedzie wykonywac export
+        $data = DB::select('EXEC ExportData ?',['doctors']);
+
+        $data= collect($data);
 
         $csvExporter = new Export();
         $csvExporter->build($data, ['id','first_name','last_name','specialization','phone_number','ward_id','created_at', 'updated_at'])->download();
     }
     public function exportWards() {
-        $data = Ward::all();//TODO:zrobic procedure ktora bedzie wykonywac export
+        $data = DB::select('EXEC ExportData ?',['wards']);
+
+        $data= collect($data);
 
         $csvExporter = new Export();
         $csvExporter->build($data, ['id', 'ward_name'])->download();
     }
     public function exportPatients() {
-        $data = Patient::all();//TODO:zrobic procedure ktora bedzie wykonywac export
+        $data = DB::select('EXEC ExportData ?',['patients']);
+
+        $data= collect($data);
 
         $csvExporter = new Export();
         $csvExporter->build($data, ['id','first_name','last_name','age','phone_number','pesel','weight','height','doctor_id','address_id','created_at', 'updated_at'])->download();
